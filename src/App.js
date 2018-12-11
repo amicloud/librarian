24fb9fa040c47c8bc14673f818cbbfe94e69b5b0
Amy Smith <amicloud.as@gmail.com>
2018-12-10 21:01:06 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PlayMusic from "playmusic";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            gEmail: '',
+            gPass: '',
+            library: []
+        };
+        this.handleChangeGooglePass = this.handleChangeGooglePass.bind(this);
+        this.handleChangeGoogleEmail = this.handleChangeGoogleEmail.bind(this);
+        this.handleSubmitGoogle = this.handleSubmitGoogle.bind(this);
+        this.getAllTracks = this.getAllTracks.bind(this);
+    }
+
+    handleChangeGoogleEmail(event){
+        this.setState({gEmail: event.target.value})
+    }
+
+    handleChangeGooglePass(event){
+        this.setState({gPass: event.target.value})
+    }
+
+
+    getAllTracks(username, password) {
+        console.log("Getting tracks");
+        let that = this;
+        console.log(username);
+        console.log(password);
+        let pm = new PlayMusic();
+        pm.login({email: username, password: password}, function (err, info) {
+            if (err) console.error(err);
+            console.log("Logged in...");
+            pm.init({androidId: info.androidId, masterToken: info.masterToken}, function (err) {
+                if (err) console.error(err);
+                console.log("Initialized music service");
+                pm.getAllTracks({limit: 100000}, function (err, library) {
+                    console.log("Got all tracks");
+                    that.setState({library: library.data.items})
+                });
+            });
+        });
+    }
+
+
+    handleSubmitGoogle(event) {
+        event.preventDefault();
+        this.getAllTracks(this.state.gEmail, this.state.gPass);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+
+                </header>
+                <div className="container">
+                    <div className="row">
+                        <h1 className="center">Librarian</h1>
+                    </div>
+                    <div className="row">
+                        <p>In the fields below, input the login information for the Google Account you wish to retrieve
+                            the library
+                            information for. The Google account also needs to have the "Allow less secure apps" setting
+                            set to "ON".
+                        </p>
+                        <form onSubmit={this.handleSubmitGoogle}>
+                            Email
+                            <input type="text" id="googleEmail" value={this.state.gEmail} onChange={this.handleChangeGoogleEmail}/>
+                            Password
+                            <input type="password" id="googlePassword" value={this.state.gPass} onChange={this.handleChangeGooglePass}/>
+                            <input type='submit' value='Get Library Data'/>
+                        </form>
+                    </div>
+                    <div className="row">
+                        <p>{this.state.library}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
